1b7f4a990ff5d76c0a532af1f7f6959c0fefda3f
JellyLiu2001 <79320181+JellyLiu2001@users.noreply.github.com>
2023-02-18 15:06:09 +0000

    deg tool finnish

--- finnal test/1/1.ino
+++ finnal test/1/1.ino
@@ -2,12 +2,12 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
 int pos = 0;
 Servo servo_9;
+Servo servo_10;
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
 void setup()
 {
-  Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
   
   /* Initialise the sensor */
@@ -17,45 +17,46 @@ void setup()
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
-  
   delay(1000);
-    
   bno.setExtCrystalUse(true);
+  Serial.begin(9600);
   servo_9.attach(9, 500, 2500);
-}
-void loop()
-{
+  servo_10.attach(10, 500, 2500);
   // sweep the servo from 0 to 180 degrees in steps
   // of 1 degrees
-  for (pos = 0; pos <= 180; pos += 5) 
+  for (pos = 0; pos <= 180; pos += 5)
   {
 
     // tell servo to go to position in variable 'pos'
     servo_9.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
-    Serial.println(pos);
-    sensors_event_t event; 
-    bno.getEvent(&event);
-    Serial.print("\tY: ");
-    Serial.print(event.orientation.y, 4);
-    Serial.print("\tZ: ");
-    Serial.print(event.orientation.z, 4);
-    Serial.println("");
+    Serial.print(pos);
+    Serial.print(",");
+    //Serial.print("Y: ");
+    Serial.print("event.orientation.y");
+    Serial.print(",");
+    //Serial.print("Z: ");
+    Serial.println("event.orientation.z");
+    delay(10);
   }
-  for (pos = 180; pos >= 0; pos -= 5) 
+  for (pos = 180; pos >= 0; pos -= 5)
   {
     // tell servo to go to position in variable 'pos'
-    servo_9.write(pos);
+    // tell servo to go to position in variable 'pos'
+    servo_10.write(pos);
     // wait 15 ms for servo to reach the position
     delay(15); // Wait for 15 millisecond(s)
-    Serial.println(pos);
-    sensors_event_t event; 
-    bno.getEvent(&event);
-    Serial.print("\tY: ");
-    Serial.print(event.orientation.y, 4);
-    Serial.print("\tZ: ");
-    Serial.print(event.orientation.z, 4);
-    Serial.println("");
+    Serial.print(pos);
+    Serial.print(",");
+    //Serial.print("Y: ");
+    Serial.print("event.orientation.y");
+    Serial.print(",");
+    //Serial.print("Z: ");
+    Serial.println("event.orientation.z");
+    delay(10);
   }
+Serial.print("finish");
 }
+void loop()
+{}
