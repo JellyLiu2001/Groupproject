e583c42ab7b02cfcac2aa40d8875a103f2656507
JellyLiu2001 <79320181+JellyLiu2001@users.noreply.github.com>
2023-02-19 01:15:38 +0000

    testing communication tools

--- communication/Pythoncommu.py
+++ communication/Pythoncommu.py
@@ -1,18 +1,19 @@
-# coding:utf-8
+import serial
 
-import serial.tools.list_ports
-import time
-plist = list(serial.tools.list_ports.comports())
+serialPort = "COM8"  # 串口
+baudRate = 9600  # 波特率
+ser = serial.Serial(serialPort, baudRate, timeout=0.5)
+print("参数设置：串口=%s ，波特率=%d" % (serialPort, baudRate))
 
-if len(plist) <= 0:
-        print("没有发现端口!")
-else:
-        plist_0 = list(plist[0])
-        serialName = plist_0[0]
-        serialFd = serial.Serial(serialName, 9600, timeout=60)
-        print("可用端口名>>>", serialFd.name)
-        while 1:
-                serialFd.write("o".encode())
-                time.sleep(1)
-                serialFd.write("c.encode())
-                time.sleep(1)
+demo1=b"0"#将0转换为ASCII码方便发送
+demo2=b"1"#同理
+while 1:
+    val = ser.readline().decode('utf-8')
+    # ser.readline() 读取窗串口中的数据以二进制的形式展示需要使用.decode('utf-8')进行解码
+    print(val)
+    c=input('请输入指令:')
+    c=ord(c)#将c转换为UTF-8标准数字
+    if(c==48):
+        ser.write(demo1)#ser.write在于向串口中写入数据
+    if(c==49):
+        ser.write(demo2)
